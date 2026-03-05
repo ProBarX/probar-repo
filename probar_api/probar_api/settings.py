183a6f089fe2f4e189ed714f5ec332550677bd7e
Samuel Bernardo <samuelbernardo.dev@gmail.com>
2026-03-05 19:28:16 -0300

    config: configurar conexão com PostgreSQL na Render

--- probar_api/probar_api/settings.py
+++ probar_api/probar_api/settings.py
@@ -1,6 +1,7 @@
 from pathlib import Path
 import os
 import environ
+import dj_database_url
 
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -70,14 +71,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'probar_api.wsgi.application'
 
 
-
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(default=env('DATABASE_URL'))
 }
-
 
 
 AUTH_PASSWORD_VALIDATORS = [
