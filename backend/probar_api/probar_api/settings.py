f6ad6e4ddde01677f1dca328d049494c9a209a7e
Samuel Bernardo <samuelbernardo.dev@gmail.com>
2026-03-25 10:06:46 -0300

    config: define data/time formats and adjust JWT settings.

--- backend/probar_api/probar_api/settings.py
+++ backend/probar_api/probar_api/settings.py
@@ -49,6 +49,17 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
+
+    "DATETIME_FORMAT": "%Y-%m-%d %H:%M:%S",
+    "DATETIME_INPUT_FORMATS": [
+        "%Y-%m-%d %H:%M:%S",  # 2025-10-20 19:30:00
+        "%Y-%m-%d %H:%M"      # 2025-10-20 19:30
+    ],
+}
+
+
+SIMPLE_JWT = {
+    "AUTH_HEADER_TYPES": ("Bearer",),
 }
 
 
