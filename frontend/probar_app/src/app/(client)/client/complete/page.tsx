80bc9099e254a71f5d7d58d9b78d8afc957b364a
Samuel Bernardo <samuelbernardo.dev@gmail.com>
2026-04-30 15:50:49 -0300

    fix: corrigir erro ao logar com google

--- frontend/probar_app/src/app/(client)/client/complete/page.tsx
+++ frontend/probar_app/src/app/(client)/client/complete/page.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from "react"
 import { useRouter } from "next/navigation"
 import { CompleteRegisterForm } from "@/components/client/CompleteRegisterForm"
 import { kaushan } from "@/fonts"
-import { api } from "@/lib/api" // 👈 usa seu axios
+import { api } from "@/services/api" 
 
 export default function CompletePage() {
   const router = useRouter()
